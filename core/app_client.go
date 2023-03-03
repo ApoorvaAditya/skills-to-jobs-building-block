0da73e59e1cbe24cc5ccdd046b9edd2f56fd22e2
Apoorva Aditya <apoorva9@illinois.edu>
2023-03-03 10:58:37 -0600

    Implemented interface methods on appClient to get bessi data using the storage interface

--- core/app_client.go
+++ core/app_client.go
@@ -14,7 +14,13 @@
 
 package core
 
-import "application/core/model"
+import (
+	"application/core/model"
+
+	"github.com/google/uuid"
+	"github.com/rokwire/logging-library-go/v2/errors"
+	"github.com/rokwire/logging-library-go/v2/logutils"
+)
 
 // appClient contains client implementations
 type appClient struct {
@@ -24,6 +30,31 @@ type appClient struct {
 // GetExample gets an Example by ID
 func (a appClient) GetExample(orgID string, appID string, id string) (*model.Example, error) {
 	return a.app.shared.getExample(orgID, appID, id)
+}
+
+// GetBessiData gets an BessiData by ID
+func (a appClient) GetBessiData(id string) (*model.BessiData, error) {
+	return a.app.storage.GetBessiData(id)
+}
+
+// CreateBessiData creates a new BessiData
+func (a appClient) CreateBessiData(example model.BessiData) (*model.BessiData, error) {
+	example.ID = uuid.NewString()
+	err := a.app.storage.CreateBessiData(example)
+	if err != nil {
+		return nil, errors.WrapErrorAction(logutils.ActionCreate, model.TypeBessiData, nil, err)
+	}
+	return &example, nil
+}
+
+// UpdateBessiData updates an BessiData
+func (a appClient) UpdateBessiData(example model.BessiData) error {
+	return a.app.storage.UpdateBessiData(example)
+}
+
+// DeleteBessiData deletes an BessiData by ID
+func (a appClient) DeleteBessiData(id string) error {
+	return a.app.storage.DeleteBessiData(id)
 }
 
 // newAppClient creates new appClient
