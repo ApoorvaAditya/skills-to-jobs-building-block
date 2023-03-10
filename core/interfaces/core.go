52ee975b453ff6819a32f1d6fba96c4f1889e82d
Apoorva Aditya <apoorva9@illinois.edu>
2023-03-10 09:46:48 -0600

    Added interface methods to expose user data APIs for the client

--- core/interfaces/core.go
+++ core/interfaces/core.go
@@ -24,6 +24,12 @@ type Default interface {
 // Client exposes client APIs for the driver adapters
 type Client interface {
 	GetExample(orgID string, appID string, id string) (*model.Example, error)
+
+	// UserData APIs
+	GetUserData(id string) (*model.UserData, error)
+	CreateUserData(example model.UserData) (*model.UserData, error)
+	UpdateUserData(example model.UserData) error
+	DeleteUserData(id string) error
 }
 
 // Admin exposes administrative APIs for the driver adapters
